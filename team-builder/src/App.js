62ef7b3d45cd115e658864aa3e1b628b202be08c
Drew Ross <mail.drew.ross@gmail.com>
2020-05-19 13:06:44 -0700

    Create working form component. Add state hooks.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,12 +2,37 @@ import React, { useState } from 'react';
 import './App.css';
 import TeamList from './components/TeamList';
 import TeamListForm from './components/TeamListForm';
+import { v4 as uuid } from 'uuid';
 
 function App() {
+
+  const initialFormValues = {
+    name: '',
+    email: '',
+    role: '',
+  }
+  const [teamList, setTeamList] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    //guard against empty submissions
+    const teamMember = { ...formValues, id: uuid() }
+    setTeamList([...teamList, teamMember]);
+    setFormValues(initialFormValues);
+  }
+
+  const onInputChange = e => {
+    const { name } = e.target;
+    const { value } = e.target;
+    const newFormValues = { ...formValues, [name]: value }
+    setFormValues(newFormValues);
+  }
+
   return (
     <div className="App">
-      <TeamListForm />
-      <TeamList />
+      <TeamListForm formValues={formValues} onInputChange={onInputChange} onSubmit={onSubmit} />
+      <TeamList teamList={teamList} />
     </div>
   );
 }
--- team-builder/src/components/TeamListForm.jsx
+++ team-builder/src/components/TeamListForm.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
 
-const TeamListForm = (props) => {
+const TeamListForm = ({ formValues, onInputChange, onSubmit }) => {
 
     return (
-        null
+        <form onSubmit={onSubmit}>
+            <label>Name:
+                <input
+                    name='name'
+                    type='text'
+                    placeholder='Full Name'
+                    maxLength='50'
+                    value={formValues.name}
+                    onChange={onInputChange}
+                />
+            </label><br />
+            <label>Email:
+                <input
+                    name='email'
+                    type='email'
+                    placeholder='user@email.com'
+                    maxLength='50'
+                    value={formValues.email}
+                    onChange={onInputChange}
+                />
+            </label><br />
+            <label>Role:
+                <select
+                    name='role'
+                    value={formValues.role}
+                    onChange={onInputChange}
+                >
+                    <option value=''>Select Role</option>
+                    <option>Frontend Engineer</option>
+                    <option>Backend Engineer</option>
+                    <option>Designer</option>
+                    <option>UX</option>
+                </select>
+            </label><br />
+            <button>Submit</button>
+        </form>
     )
 }
 
